dcc408de7a803c8968f639846a84f0ff4a30e058
Ulf Slunga <ulf.slunga@sinetiq.se>
2026-01-26 13:50:34 +0100

    Config file

---
+++ src/main/java/se/fk/rimfrost/framework/logic/config/RegelConfig.java
@@ -0,0 +1,36 @@
+package se.fk.rimfrost.framework.logic.config;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+public class RegelConfig
+{
+
+   private Uppgift uppgift;
+
+   // REQUIRED by SnakeYAML
+   @SuppressWarnings("unused")
+   public RegelConfig()
+   {
+   }
+
+   @SuppressWarnings("unused")
+   @SuppressFBWarnings("EI_EXPOSE_REP")
+   public RegelConfig(Uppgift uppgift)
+   {
+      this.uppgift = uppgift;
+   }
+
+   @SuppressWarnings("unused")
+   @SuppressFBWarnings("EI_EXPOSE_REP")
+   public Uppgift getUppgift()
+   {
+      return uppgift;
+   }
+
+   @SuppressWarnings("unused")
+   @SuppressFBWarnings("EI_EXPOSE_REP")
+   public void setUppgift(Uppgift uppgift)
+   {
+      this.uppgift = uppgift;
+   }
+}
