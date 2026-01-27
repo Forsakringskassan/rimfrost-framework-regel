f2e6c34ee5c6947a82f82ca2269db6a6403e717a
Ulf Slunga <ulf.slunga@sinetiq.se>
2026-01-27 08:23:11 +0100

    uppdaterar RegelConfig från rimfrost-common

--- src/main/java/se/fk/rimfrost/framework/logic/config/RegelConfig.java
+++ src/main/java/se/fk/rimfrost/framework/logic/config/RegelConfig.java
@@ -2,35 +2,73 @@ package se.fk.rimfrost.framework.logic.config;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
+@SuppressWarnings("unused")
 public class RegelConfig
 {
 
    private Uppgift uppgift;
+   private Specifikation specifikation;
+   private Regel regel;
+   private Lagrum lagrum;
 
-   // REQUIRED by SnakeYAML
-   @SuppressWarnings("unused")
    public RegelConfig()
    {
    }
 
-   @SuppressWarnings("unused")
-   @SuppressFBWarnings("EI_EXPOSE_REP")
-   public RegelConfig(Uppgift uppgift)
+   @SuppressFBWarnings("EI_EXPOSE_REP2")
+   public RegelConfig(Uppgift uppgift, Specifikation specifikation, Regel regel, Lagrum lagrum)
    {
       this.uppgift = uppgift;
+      this.specifikation = specifikation;
+      this.regel = regel;
+      this.lagrum = lagrum;
    }
 
-   @SuppressWarnings("unused")
    @SuppressFBWarnings("EI_EXPOSE_REP")
    public Uppgift getUppgift()
    {
       return uppgift;
    }
 
-   @SuppressWarnings("unused")
    @SuppressFBWarnings("EI_EXPOSE_REP")
    public void setUppgift(Uppgift uppgift)
    {
       this.uppgift = uppgift;
+   }
+
+   @SuppressFBWarnings("EI_EXPOSE_REP")
+   public Specifikation getSpecifikation()
+   {
+      return specifikation;
+   }
+
+   @SuppressFBWarnings("EI_EXPOSE_REP")
+   public void setSpecifikation(Specifikation specifikation)
+   {
+      this.specifikation = specifikation;
+   }
+
+   @SuppressFBWarnings("EI_EXPOSE_REP")
+   public Regel getRegel()
+   {
+      return regel;
+   }
+
+   @SuppressFBWarnings("EI_EXPOSE_REP")
+   public void setRegel(Regel regel)
+   {
+      this.regel = regel;
+   }
+
+   @SuppressFBWarnings("EI_EXPOSE_REP")
+   public Lagrum getLagrum()
+   {
+      return lagrum;
+   }
+
+   @SuppressFBWarnings("EI_EXPOSE_REP")
+   public void setLagrum(Lagrum lagrum)
+   {
+      this.lagrum = lagrum;
    }
 }
