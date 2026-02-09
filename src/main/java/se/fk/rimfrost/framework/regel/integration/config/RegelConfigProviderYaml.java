ba2df031beb4fafada953452d74155a0e92e41b0
Lars Persson <254442132+larpersso@users.noreply.github.com>
2026-02-09 10:02:16 +0100

    fix: Rework config discovery handling
    This commit reworks the config discovery handling to rely
    on application.properties setting and environment variable
    instead of classpath and environment variable for config
    discovery.
    
    This enables the possibility to specify an alternative
    config.yaml for testing purposes.

--- src/main/java/se/fk/rimfrost/framework/regel/integration/config/RegelConfigProviderYaml.java
+++ src/main/java/se/fk/rimfrost/framework/regel/integration/config/RegelConfigProviderYaml.java
@@ -4,6 +4,7 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.quarkus.runtime.Startup;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import se.fk.rimfrost.framework.regel.logic.config.RegelConfig;
 import java.nio.file.Path;
 
@@ -14,24 +15,23 @@ public class RegelConfigProviderYaml implements RegelConfigProvider
 {
 
    private static final String ENV_CONFIG_PATH = "REGEL_CONFIG_PATH";
-   private static final String DEFAULT_CONFIG_PATH = "src/main/resources/config.yaml"; // local default
 
    private RegelConfig config;
+
+   @ConfigProperty(name = "application.config.path")
+   String applicationConfigPath;
 
    @PostConstruct
    void init()
    {
       String configPath = System.getenv(ENV_CONFIG_PATH);
-      if (configPath == null || configPath.isBlank())
+
+      if (configPath == null || configPath.isEmpty())
       {
-         this.config = YamlConfigLoader.loadFromClasspath(
-               "config.yaml",
-               RegelConfig.class);
+         configPath = applicationConfigPath;
       }
-      else
-      {
-         this.config = YamlConfigLoader.loadFromFile(Path.of(configPath), RegelConfig.class);
-      }
+
+      this.config = YamlConfigLoader.loadFromFile(Path.of(configPath), RegelConfig.class);
    }
 
    @SuppressFBWarnings("EI_EXPOSE_REP")
