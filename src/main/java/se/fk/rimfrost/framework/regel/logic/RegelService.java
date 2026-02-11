eb275eb72aad8971639f5348bc9b7d28aab37b4f
Ulf Slunga <ulf.slunga@sinetiq.se>
2026-02-11 14:16:44 +0100

    fix: Lägger till kundbehovsflöde adapter

--- src/main/java/se/fk/rimfrost/framework/regel/logic/RegelService.java
+++ src/main/java/se/fk/rimfrost/framework/regel/logic/RegelService.java
@@ -5,10 +5,11 @@ import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import se.fk.rimfrost.framework.kundbehovsflode.adapter.KundbehovsflodeAdapter;
+import se.fk.rimfrost.framework.kundbehovsflode.adapter.KundbehovsflodeMapper;
 import se.fk.rimfrost.framework.regel.Utfall;
 import se.fk.rimfrost.framework.regel.integration.config.RegelConfigProviderYaml;
 import se.fk.rimfrost.framework.regel.integration.kafka.RegelKafkaProducer;
-import se.fk.rimfrost.framework.regel.integration.kundbehovsflode.KundbehovsflodeAdapter;
 import se.fk.rimfrost.framework.regel.logic.config.RegelConfig;
 import se.fk.rimfrost.framework.regel.logic.dto.RegelDataRequest;
 import se.fk.rimfrost.framework.regel.logic.entity.*;
@@ -27,6 +28,9 @@ public class RegelService implements RegelRequestHandlerInterface
 
    @Inject
    protected RegelMapper regelMapper;
+
+   @Inject
+   protected KundbehovsflodeMapper kundbehovsflodeMapper;
 
    @Inject
    protected KundbehovsflodeAdapter kundbehovsflodeAdapter;
