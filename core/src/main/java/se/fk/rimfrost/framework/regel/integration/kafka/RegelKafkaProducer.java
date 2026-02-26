76842e9029bb162a786bd115874ebb76aa01bcfa
Nils Elveros <nils.elveros@tromb.com>
2026-02-26 11:05:34 +0100

    fix: added logger to kafkaproducer

--- core/src/main/java/se/fk/rimfrost/framework/regel/integration/kafka/RegelKafkaProducer.java
+++ core/src/main/java/se/fk/rimfrost/framework/regel/integration/kafka/RegelKafkaProducer.java
@@ -1,7 +1,11 @@
 package se.fk.rimfrost.framework.regel.integration.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.OnOverflow;
@@ -11,6 +15,8 @@ import se.fk.rimfrost.framework.regel.RegelResponseMessagePayload;
 @ApplicationScoped
 public class RegelKafkaProducer
 {
+   private static final Logger LOGGER = LoggerFactory.getLogger(RegelKafkaProducer.class);
+
    @Inject
    RegelKafkaMapper mapper;
 
@@ -19,9 +25,13 @@ public class RegelKafkaProducer
    @OnOverflow(value = OnOverflow.Strategy.BUFFER, bufferSize = 1024)
    Emitter<RegelResponseMessagePayload> regelResponseEmitter;
 
+   @ConfigProperty(name = "mp.messaging.outgoing.regel-responses.topic")
+   String topic;
+
    public void sendRegelResponse(RegelResponse regelResponse)
    {
       var response = mapper.toRegelResponseMessagePayload(regelResponse);
+      LOGGER.info("Sending RegelResponse for kundbehovsflode {} to topic {}", regelResponse.kundbehovsflodeId(), topic);
       regelResponseEmitter.send(response);
    }
 
