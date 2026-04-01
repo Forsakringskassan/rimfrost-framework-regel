6886a9fa4d4e82c72690b854a1b2cf2305ca8704
Nils Elveros <nils.elveros@tromb.com>
2026-04-01 10:40:33 +0200

    fix: add createProduceratResultatData to RegelUtils

--- core/src/main/java/se/fk/rimfrost/framework/regel/logic/RegelUtils.java
+++ core/src/main/java/se/fk/rimfrost/framework/regel/logic/RegelUtils.java
@@ -84,5 +84,23 @@ public class RegelUtils
          throw new InternalError("Could not parse object to String", e);
       }
    }
+   
+   /***
+    * 
+    * @param object Objekt som ska bli till json-format
+    * @param objectMapper Används för att mappa object till json
+    * @return object i json-format
+    */
+   public static String createProduceratResultatData(Object object, ObjectMapper objectMapper)
+   {
+      try
+      {
+         return objectMapper.writeValueAsString(object);
+      }
+      catch (JsonProcessingException e)
+      {
+         throw new InternalError("Could not parse object to String", e);
+      }
+   }
 
 }
