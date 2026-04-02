53b758359a42159b83b0a05067e6e0b32477452e
Ulf Slunga <ulf.slunga@sinetiq.se>
2026-04-02 09:39:38 +0200

    fix: handlaggning adapter 0.1.16

--- core/src/main/java/se/fk/rimfrost/framework/regel/logic/RegelUtils.java
+++ core/src/main/java/se/fk/rimfrost/framework/regel/logic/RegelUtils.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
