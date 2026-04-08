41316efca88c9ac349cc7512b42a3ccf35d3e471
Lars Persson <254442132+larpersso@users.noreply.github.com>
2026-04-08 12:47:51 +0200

    fix: Bump rimfrost-framework-handlaggning-adapter version

--- core/src/main/java/se/fk/rimfrost/framework/regel/logic/RegelRequestHandlerBase.java
+++ core/src/main/java/se/fk/rimfrost/framework/regel/logic/RegelRequestHandlerBase.java
@@ -4,7 +4,6 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
